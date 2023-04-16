9dbba15b629a085401539fc0f8c99737eea14574
Iago <iagogaraujo@gmail.com>
2023-04-16 11:40:02 -0300

    Limpando banido.test.js

--- Backend/tests/banido.test.js
+++ Backend/tests/banido.test.js
@@ -3,80 +3,65 @@ const { app, server } = require("../index");
 const db = require("../src/models");
 
 describe("BANIDO API", () => {
-  let usuario;
-  let grupo;
-  let banido;
+  let createdGrupo, createdUsuario, createdUsuarioGrupo, createdBanido;
   let logMessages = [];
 
   beforeAll(async () => {
-    // console.log = (message) => {
-    //   logMessages.push(message);
-    // };
+    console.log = (message) => {
+      logMessages.push(message);
+    };
 
-    // Garantia de que o usuário não existe
-    const usuarioCheck = await request(app).post("/api/usuario/search/").send({
-      Email: "test@gmail.com",
-    });
+    let res;
 
-    if (usuarioCheck.statusCode === 200) {
-      await request(app).delete(`/api/usuario/${usuarioCheck.body[0].ID}`);
-    }
-
-    // Garantia de que o grupo não existe
-    const grupoCheck = await request(app).post("/api/grupo/search/").send({
-      Nome: "Teste",
-    });
-
-    if (grupoCheck.statusCode === 200) {
-      await request(app).delete(`/api/grupo/${grupoCheck.body[0].ID}`);
-    }
-
-    // Criação do usuário
-    const usuarioRes = await request(app).post("/api/usuario/").send({
+    // * Dados para os testes
+    const usuario = {
       Nome: "Teste",
       Email: "teste@gmail.com",
       Senha: "123456",
       Salt: "test123",
       CPF: "12345678910",
-      ID_Instituicao: 1,
-    });
-    usuario = usuarioRes.body;
+      ID_Instituicao: 1, // * Sempre vai existir.
+    };
 
-    // Criação do grupo
-    const grupoRes = await request(app).post("/api/grupo/").send({
+    const grupo = {
       Nome: "Teste",
       Categoria: "Teste",
       Privado: "false",
-    });
-    grupo = grupoRes.body;
+    };
 
-    // Associação do usuário ao grupo
-    await request(app).post("/api/usuarioGrupo/").send({
-      ID_Usuario: usuario.ID,
-      ID_Grupo: grupo.ID,
+    res = await request(app).post("/api/usuario").send(usuario);
+    if (res.statusCode !== 201) process.exit(1);
+    createdUsuario = res.body;
+
+    res = await request(app).post("/api/grupo").send(grupo);
+    if (res.statusCode !== 201) process.exit(1);
+    createdGrupo = res.body;
+
+    // * Associação do usuário ao grupo
+    // * É necessário para gerar um banimento
+    res = await request(app).post("/api/usuarioGrupo/").send({
+      ID_Usuario: createdUsuario.ID,
+      ID_Grupo: createdGrupo.ID,
     });
+    if (res.statusCode !== 201) process.exit(1);
+    createdUsuarioGrupo = res.body;
   });
 
   it("deve criar um novo banimento", async () => {
-    console.log(`
-    ID_Usuario: ${usuario.ID}
-    ID_Grupo: ${grupo.ID}
-    `);
-
     const res = await request(app).post("/api/banido/").send({
-      ID_Usuario: usuario.ID,
-      ID_Grupo: grupo.ID,
+      ID_Usuario: createdUsuario.ID,
+      ID_Grupo: createdGrupo.ID,
     });
-
-    banido = res.body;
 
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("ID");
+
+    createdBanido = res.body;
   });
 
   it("não deve criar um novo banimento com um grupo que não existe", async () => {
     const res = await request(app).post("/api/banido/").send({
-      ID_Usuario: usuario.ID,
+      ID_Usuario: createdUsuario.ID,
       ID_Grupo: -1,
     });
 
@@ -86,7 +71,7 @@ describe("BANIDO API", () => {
   it("não deve criar um novo banimento com um usuário que não existe", async () => {
     const res = await request(app).post("/api/banido/").send({
       ID_Usuario: -1,
-      ID_Grupo: grupo.ID,
+      ID_Grupo: createdGrupo.ID,
     });
 
     expect(res.statusCode).toEqual(500);
@@ -94,8 +79,8 @@ describe("BANIDO API", () => {
 
   it("não deve criar um novo banimento com um usuário que já está banido", async () => {
     const res = await request(app).post("/api/banido/").send({
-      ID_Usuario: usuario.ID,
-      ID_Grupo: grupo.ID,
+      ID_Usuario: createdUsuario.ID,
+      ID_Grupo: createdGrupo.ID,
     });
 
     expect(res.statusCode).toEqual(400);
@@ -107,7 +92,7 @@ describe("BANIDO API", () => {
   });
 
   it("deve retornar um banimento", async () => {
-    const res = await request(app).get(`/api/banido/${banido.ID}`);
+    const res = await request(app).get(`/api/banido/${createdBanido.ID}`);
     expect(res.statusCode).toEqual(200);
   });
 
@@ -117,7 +102,7 @@ describe("BANIDO API", () => {
   });
 
   it("deve deletar um banimento", async () => {
-    const res = await request(app).delete(`/api/banido/${banido.ID}`);
+    const res = await request(app).delete(`/api/banido/${createdBanido.ID}`);
     expect(res.statusCode).toEqual(200);
   });
 
@@ -127,9 +112,10 @@ describe("BANIDO API", () => {
   });
 
   afterAll(async () => {
-    // Deletar o usuário e grupo criados
-    await request(app).delete(`/api/usuario/${usuario.ID}`);
-    await request(app).delete(`/api/grupo/${grupo.ID}`);
+    // * Limpeza dos dados criados
+    await request(app).delete(`/api/usuarioGrupo/${createdUsuarioGrupo.ID}`);
+    await request(app).delete(`/api/usuario/${createdUsuario.ID}`);
+    await request(app).delete(`/api/grupo/${createdGrupo.ID}`);
 
     await db.sequelize.close();
     await server.close();
