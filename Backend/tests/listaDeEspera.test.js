d2713b1bf25cd499f15aed74dd3df407b4217dc4
Iago <iagogaraujo@gmail.com>
2023-04-19 10:22:51 -0300

    Adicionando testes no listaDeEspera.test.js

--- Backend/tests/listaDeEspera.test.js
+++ Backend/tests/listaDeEspera.test.js
@@ -58,6 +58,51 @@ describe("LISTA_DE_ESPERA API", () => {
     createdListaDeEspera = res.body;
   });
 
+  it("não deve criar uma lista de espera com string no ID_Usuario", async () => {
+    const res = await request(app).post("/api/listaDeEspera/").send({
+      ID_Instituicao: createdInstituicao.ID,
+      ID_Usuario: "string",
+    });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("não deve criar uma lista de espera com string no ID_Instituicao", async () => {
+    const res = await request(app).post("/api/listaDeEspera/").send({
+      ID_Instituicao: "string",
+      ID_Usuario: createdUsuario.ID,
+    });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("não deve criar uma lista de espera com um usuário que não existe", async () => {
+    const res = await request(app).post("/api/listaDeEspera/").send({
+      ID_Instituicao: createdInstituicao.ID,
+      ID_Usuario: 999
+    });
+
+    expect(res.statusCode).toEqual(500);
+  });
+
+  it("não deve criar uma lista de espera com uma instituição que não existe", async () => {
+    const res = await request(app).post("/api/listaDeEspera/").send({
+      ID_Instituicao: 999,
+      ID_Usuario: createdUsuario.ID
+    });
+
+    expect(res.statusCode).toEqual(500);
+  });
+
+  it("não deve criar uma lista de espera com um usuário que já está na lista de espera", async () => {
+    const res = await request(app).post("/api/listaDeEspera/").send({
+      ID_Instituicao: createdInstituicao.ID,
+      ID_Usuario: createdUsuario.ID
+    });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
   it("deve retornar todas as listas de espera", async () => {
     const res = await request(app).get("/api/listaDeEspera");
     expect(res.statusCode).toEqual(200);
@@ -72,7 +117,18 @@ describe("LISTA_DE_ESPERA API", () => {
     expect(res.body.ID).toEqual(createdListaDeEspera.ID);
   });
 
-  // ! Já deleta a lista de espera criada, então não precisa deletar depois
+  it("deve atualizar uma lista de espera", async () => {
+    const updatedInstituicao = 1;
+    const res = await request(app)
+      .put(`/api/listaDeEspera/${createdListaDeEspera.ID}`)
+      .send({
+        ID_Instituicao: updatedInstituicao,
+        ID_Usuario: createdUsuario.ID,
+      });
+
+      expect(res.statusCode).toEqual(200);
+  });
+
   it("deve deletar uma lista de espera", async () => {
     const res = await request(app).delete(
       `/api/listaDeEspera/${createdListaDeEspera.ID}`
