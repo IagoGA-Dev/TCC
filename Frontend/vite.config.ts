7f8d0a61c3f6169948496905ab7aae74212660f7
Iago <iagogaraujo@gmail.com>
2023-05-27 12:25:40 -0300

    Adicionando docker ao frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 3001,
-  },
   server: {
-    port: 3001,
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true,
+    },
   },
 })
