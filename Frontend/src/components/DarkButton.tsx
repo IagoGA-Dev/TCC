923af3582d0446524968f87aff53a6c769c18d48
Iago <iagogaraujo@gmail.com>
2023-05-31 14:36:41 -0300

    Adicionando modal em Groups.tsx

--- Frontend/src/components/DarkButton.tsx
+++ Frontend/src/components/DarkButton.tsx
@@ -4,12 +4,15 @@ interface DarkButtonProps {
   icon: React.ReactNode;
   text: string;
   onClick?: () => void;
+  className?: string;
 }
 
-function DarkButton({ icon, text, onClick }: DarkButtonProps) {
+function DarkButton({ icon, text, onClick, className }: DarkButtonProps) {
   return (
     <button
-      className="flex items-center gap-2 bg-gray-200 px-4 py-2 rounded-md hover:bg-gray-300 transition duration-300 ease-in-out"
+      className={`flex items-center gap-2 bg-gray-200 px-4 py-2 rounded-md hover:bg-gray-300 transition duration-300 ease-in-out
+        ${className && className}
+      `}
       onClick={onClick}
     >
       {icon}
--- Frontend/src/pages/Menus/Groups.tsx
+++ Frontend/src/pages/Menus/Groups.tsx
@@ -8,6 +8,8 @@ import DarkButton from "../../components/DarkButton";
 import { BsPlusCircleDotted } from "react-icons/bs";
 import api from "../../services/api";
 
+import Modal from "react-modal";
+
 interface Group {
   ID: number;
   Nome: string;
@@ -72,8 +74,8 @@ function GroupCard({
 function Groups({ redirect }: { redirect: (active: string) => void }) {
   // * Estado para a barra de busca
   const [searchTerm, setSearchTerm] = useState("");
-
   const [groups, setGroups] = useState<Group[]>([]);
+  const [showModal, setShowModal] = useState(true);
 
   useEffect(() => {
     fetchGroups().then((data) => {
@@ -82,8 +84,8 @@ function Groups({ redirect }: { redirect: (active: string) => void }) {
   }, []);
 
   const filteredGroups = groups.filter((group) => {
-    const name = `Grupo ${group}`;
-    const description = `Descrição do grupo ${group}`;
+    const name = group.Nome;
+    const description = group.Descricao;
     const term = searchTerm.toLowerCase();
 
     return (
@@ -101,6 +103,7 @@ function Groups({ redirect }: { redirect: (active: string) => void }) {
         <DarkButton
           icon={<BsPlusCircleDotted className="w-6 h-6 text-gray-700" />}
           text="Novo Grupo"
+          onClick={() => setShowModal(true)}
         />
       </MenuTitle>
       <div className="py-9 bg-gray-50">
@@ -121,6 +124,83 @@ function Groups({ redirect }: { redirect: (active: string) => void }) {
           ))}
         </div>
       </div>
+
+      {/* Modal */}
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded-md outline-none shadow-lg"
+        overlayClassName="fixed inset-0 bg-gray-500 bg-opacity-75"
+      >
+        <div className="bg-white rounded-md w-96 p-4">
+          <div className="text-xl font-bold mb-2 text-center">Novo Grupo</div>
+          <hr />
+          <form className="flex flex-col gap-4 mt-3">
+            <label className="flex flex-col">
+              <span className="text-sm text-gray-500 mb-2">Nome do Grupo</span>
+              <input
+                type="text"
+                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </label>
+            <label className="flex flex-col">
+              <span className="text-sm text-gray-500 mb-2">
+                Descrição do Grupo
+              </span>
+              <input
+                type="text"
+                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </label>
+            <label className="flex flex-col">
+              <span className="text-sm text-gray-500 mb-2">Categoria</span>
+              <select
+                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                defaultValue="default"
+              >
+                <option value="default" disabled>
+                  Selecione uma categoria
+                </option>
+                <option value="1">Categoria 1</option>
+                <option value="2">Categoria 2</option>
+                <option value="3">Categoria 3</option>
+              </select>
+            </label>
+            {/* <label className="flex flex-col">
+              <span className="text-sm text-gray-500 mb-2">Imagem</span>
+              <input
+                type="file"
+                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </label> */}
+            <label className="flex flex-col">
+              {/* Temporáriamente até eu descobrir como vou fazer o upload da imagem */}
+              {/* O problema é abrir a porta para alguma vulnerabilidade */}
+              <span className="text-sm text-gray-500 mb-2">Imagem (URL)</span>
+              <input
+                type="text"
+                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </label>
+          </form>
+          <div className="flex justify-start gap-4 mt-6 font-medium font-montserrat">
+            <div className="flex-1">
+              <DarkButton icon={<></>} text="Criar"
+                onClick={() => setShowModal(false)}
+                className="w-full justify-center"
+              />
+            </div>
+            <div className="flex-1">
+              <DarkButton
+                icon={<></>}
+                text="Cancelar"
+                onClick={() => setShowModal(false)}
+                className="w-full justify-center"
+              />
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
